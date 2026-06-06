2331dec0fc84a31ec763c6d6a4ea77ce91ae5348
Shaman SR <shamansr21@gmail.com>
2026-06-06 14:12:48 +0530

    extended functionality and added ttl to put and get

--- src/kosha/main.py
+++ src/kosha/main.py
@@ -1,6 +1,5 @@
 import logging
-from pydantic import BaseModel, Field
-import uuid
+import time
 
 logger = logging.getLogger(__name__)
 
@@ -9,8 +8,9 @@ class Hashmap:
     # Get function which will return me the value in that index
     # Initially let's say my array is of size 10
     # mod of 10 to get index between 0 - 9 and also have to handle chaining to make room for more values if they have the same index
-    def __init__(self, size: int = 1000) -> None:
+    def __init__(self, size: int = 1000, default_ttl: int = 86400) -> None:
         self.size = size
+        self.default_ttl = default_ttl
 
         self.hash_map = [[] for _ in range(self.size)]
 
@@ -18,26 +18,30 @@ class Hashmap:
         if key is None:
             raise ValueError("Key must be passed")
 
-        return sum(int(char) for char in key if char.isdigit()) % self.size
+        return sum(int(char) if char.isdigit() else ord(char) for char in key) % self.size
 
-    def ttl(self, key):
-        # Set a time for the key to live I can simply define this and extend my put function with this
-        pass
+    def expire(self, ttl: int | None = None):
+        active_ttl = ttl if ttl is not None else self.default_ttl
+        expires_at = time.time() + active_ttl
 
-    def put(self, key, value):
+        return expires_at
+
+    def put(self, key, value, ttl: int | None = None):
         if value is None or key is None:
             raise TypeError("One or more argument is missing")
 
         index = self.hash_function(key)
         bucket = self.hash_map[index]
 
+        expires_at = self.expire(ttl)
+
         try:
-            for i, (k,v) in enumerate(bucket):
-                if k == key:
-                    bucket[i] = (key, value)
+            for i, item in enumerate(bucket):
+                if item[0] == key:
+                    bucket[i] = (key, value, expires_at)
                     logger.info(f"Value {value} updated in bucket {index}")
                     return
-            bucket.append((key, value))
+            bucket.append((key, value, expires_at))
             logger.info(f"Value {value} added to bucket {index}")
         except (TypeError) as e:
             logger.error(f"Error while updating or adding a value check the hashmap {e}")
@@ -47,11 +51,18 @@ class Hashmap:
             raise TypeError("One or more argument is missing")
         index = self.hash_function(key)
         bucket = self.hash_map[index]
+        current_time = time.time()
 
         try:
-            for k,v in bucket:
-                if k == key:
-                    return v
+            for item in bucket:
+                if item[0] == key:
+                    expires_at = item[2]
+
+                    if expires_at is not None and current_time < expires_at:
+                        return item[1]
+
+                    self.remove(key)
+
         except (TypeError) as e:
             logger.error(f"Error while retrieving value for the key {e}")
 
@@ -63,16 +74,10 @@ class Hashmap:
         bucket = self.hash_map[index]
 
         try:
-            for i, (k, v) in enumerate(bucket):
-                if k == key:
+            for i, item in enumerate(bucket):
+                if item[0] == key:
                     del bucket[i]
-                    logger.info(f"Value {v} removed from bucket {index}")
+                    logger.info(f"Value {item[1]} removed from bucket {index}")
                     return
         except (ValueError) as e:
             logger.error(f"Error while deleting value for the key {e}")
-
-
-class KeyValueStore(BaseModel):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4)
-    hashmap: Hashmap | None = Field(default=None)
-    ttl: int = Field(default=86400)
